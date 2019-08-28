46435004250cb0389439ce885d704844dacb9190
leolvcl <lvclleo@gmail.com>
2019-08-28 23:41:18 +0800

    创建app

---
+++ leo_typeidea/leo_typeidea/blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Category(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除'),
+    )
+    name = models.CharField(max_length=50, verbose_name='名称')
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name='状态')
+    is_nav = models.BooleanField(default=False, verbose_name='是否为导航')
+    owner = models.ForeignKey(User, verbose_name='作者')
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+
+    class Meta:
+        verbose_name = verbose_name_plural = '分类'
