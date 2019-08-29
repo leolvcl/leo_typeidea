cb09581f78bba4e16822e67eb04bd729eb9595df
leolvcl <lvclleo@gmail.com>
2019-08-29 21:10:41 +0800

    创建blog comment config 三个app

--- leo_typeidea/leo_typeidea/blog/models.py
+++ leo_typeidea/leo_typeidea/blog/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+# 每个Model定义的Meta类属性，作用是配置Model属性，配置的展示名称
+
 
 class Category(models.Model):
     STATUS_NORMAL = 1
@@ -19,3 +21,39 @@ class Category(models.Model):
 
     class Meta:
         verbose_name = verbose_name_plural = '分类'
+
+class Tag(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除'),
+    )
+    name = models.CharField(max_length=10, verbose_name='名称')
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name='状态')
+    # is_nav = models.BooleanField(default=False, verbose_name='是否为导航')
+    owner = models.ForeignKey(User, verbose_name='作者')
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    class Meta:
+        verbose_name = verbose_name_plural = '标签'
+
+class Post(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_DRAFT = 2
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除'),
+        (STATUS_DRAFT, '草稿')
+    )
+    title = models.CharField(max_length=255,verbose_name='标题')
+    desc = models.CharField(max_length=1024, blank=True,verbose_name='摘要')
+    content = models.TextField(verbose_name='正文',help_text='正文必须是markdown格式')
+    status = models.PositiveIntegerField(default=STATUS_NORMAL,choices=STATUS_ITEMS,verbose_name='状态')
+    tag = models.ManyToManyField(Tag,verbose_name='标签')
+    owner = models.ForeignKey(User,verbose_name='作者')
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+
+    class Meta:
+        verbose_name = verbose_name_plural = '文章'
+        ordering = ['-id']  # 根据id降序排列
--- leo_typeidea/leo_typeidea/leo_typeidea/settings/base.py
+++ leo_typeidea/leo_typeidea/leo_typeidea/settings/base.py
@@ -29,6 +29,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'config',
+    'comment',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
